8e76d46b16ddf05256f29b7a664870efc364ef7e
radhey2 <96431541+radhey2@users.noreply.github.com>
2022-02-03 19:41:04 +0530

    Add files via upload

---
+++ SnakeUc2.java
@@ -0,0 +1,19 @@
+package snake;
+import java.util.Random;
+public class SnakeUc2 {
+	
+	
+
+		public static void main(String[] args) {
+			   int playerPosition = 0;
+		       System.out.println("Welcome to Snake Ladder Program");
+
+		//Player Position Start With Zero
+		       System.out.println("Player Position:" + playerPosition);
+		//Roll Dice
+		       Random rand = new Random();
+		       int dice = rand.nextInt((6)+1);
+		       System.out.println("Roll Dice: " + dice);
+		    }
+		}
+
